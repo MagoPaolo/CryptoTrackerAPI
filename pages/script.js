975533832ff1b13c7dc882e2d300ab4ca2914213
Paolo <101174688+PolTheWizard@users.noreply.github.com>
2023-05-17 23:19:14 +0000

    Miglioramenti grafici

--- pages/script.js
+++ pages/script.js
@@ -1,12 +1,22 @@
 async function fetchTrending() {
     const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&locale=en");
     const trending = await response.json();
-
-    document.getElementById("coinName").innerHTML = trending[0].name
-    document.getElementById("coinRank").innerHTML = trending[0].market_cap_rank
-    document.getElementById("coinPrice").innerHTML = trending[0].current_price
-    document.getElementById("coinPercentage").innerHTML = trending[0].price_change_percentage_24h
-    document.getElementById("coinImg").src = trending[0].image
-    document.getElementById("coin1Name").innerHTML = trending[1].name
-    document.getElementById("coin1Img").src = trending[1].image
+    assignTrending(trending);
 }
+
+function assignTrending(trending) {
+    for (let i = 1; i < 4; i++) {
+        document.getElementById("trendingTitle" + i).innerHTML = trending[i-1].name
+        document.getElementById("trendingPrice" + i).innerHTML = trending[i-1].current_price
+        document.getElementById("trendingChangeP" + i).innerHTML = trending[i-1].price_change_percentage_24h.toFixed(2)
+        document.getElementById("trendingChange" + i).innerHTML = trending[i-1].price_change_24h.toFixed(2)
+        document.getElementById("trendingImg" + i).src = trending[i-1].image
+    }
+}
+
+
+
+
+
+
+
